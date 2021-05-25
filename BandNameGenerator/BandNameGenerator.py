4d532c45e38a2722ab2ef8dd5b0107c5058b76e4
Erik White <ejwhite92@gmail.com>
2021-05-25 04:50:59 -0700

    Initial commit

---
+++ BandNameGenerator/BandNameGenerator.py
@@ -0,0 +1,8 @@
+# 1. Greet users.
+print('Welcome to the Band Name Generator!')
+# 2. Ask user for the name of the city in which they grew up.
+city = input('What\'s the name of your hometown?\n')
+# 3. Ask the user for the name of a pet.
+pet = input('What\'s the name of your first pet?\n')
+# 4. Combine the city and pet to make a band name.
+print('Have you considered the name ' + city + " " + pet + ' for your band?')
