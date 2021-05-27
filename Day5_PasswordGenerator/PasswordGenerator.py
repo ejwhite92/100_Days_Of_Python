74823c000d94bf65205077a3e27e982f9ad11c58
Erik White <ejwhite92@gmail.com>
2021-05-27 10:22:26 -0700

    Add Day 5 and Various dir

---
+++ Day5_PasswordGenerator/PasswordGenerator.py
@@ -0,0 +1,31 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+
+for char in range(1, nr_letters + 1):
+    rand_char = random.choice(letters)
+    password += rand_char
+
+for num in range(1, nr_numbers + 1):
+    rand_num = random.choice(numbers)
+    password += rand_num
+
+for sym in range(1, nr_symbols + 1):
+    rand_sym = random.choice(symbols)
+    password += rand_sym
+
+random.shuffle(password)
+new_password = ""
+for char in password:
+    new_password += char
+
+print(f'Your password is: {new_password}')
---
+++ Various/FizzBuzz.py
@@ -0,0 +1,9 @@
+for n in range(1, 101):
+    if n % 3 == 0 and n % 5 == 0:
+        print('fizzbuzz')
+    elif n % 3 == 0:
+        print('fizz')
+    elif n % 5 == 0:
+        print('buzz')
+    else:
+        print(n)
