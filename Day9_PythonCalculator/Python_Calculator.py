4dbd190b519137210e15875aa84311a0fb1043b4
Erik White <ejwhite92@gmail.com>
2021-06-01 07:00:53 -0700

    add Day 9 code

---
+++ Day9_PythonCalculator/Python_Calculator.py
@@ -0,0 +1,61 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def calculator():
+    should_continue = True
+    num1 = float(input("Input your first number: "))
+    while should_continue:
+        choice = input("Pick a symbol: ")
+        num2 = float(input("Input your second number: "))
+        function = operations[choice]
+        answer = function(num1, num2)
+        print(f'{num1} {choice} {num2} = {answer}')
+
+        if input(f'Would you like to perform another operation on {answer}? Type "Y" for Yes and "N" for No. ') == "Y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+print(logo)
+for symbol in operations:
+    print(symbol)
+calculator()
